ca2d1ff8a7381732f7b2ebf062121d8ed3bc70f7
Tomasz Wiszkowski <tomasz.wiszkowski@gmail.com>
2024-11-28 19:57:27 -0800

    Add CRC tests
    Change-Id: I381762cabdd6ab48e6789b54211ba097685767ef

---
+++ src/unlzx_crc_test.cc
@@ -0,0 +1,39 @@
+#include "gtest/gtest.h"
+#include "unlzx.h"
+
+namespace {
+
+TEST(CrcCalcTest, HandlesZeroLength) {
+    uint8_t memory[1] = {0};
+    crc::reset();
+    ASSERT_EQ(crc::calc(memory, 0), 0);
+    ASSERT_EQ(crc::sum(), 0);
+}
+
+TEST(CrcCalcTest, HandlesSingleByte) {
+    uint8_t memory[1] = {0x01};
+    crc::reset();
+    ASSERT_EQ(crc::calc(memory, 1), 0xA505DF1B);
+    ASSERT_EQ(crc::sum(), 0xA505DF1B);
+}
+
+TEST(CrcCalcTest, HandlesMultipleBytes) {
+    uint8_t memory[3] = {0x01, 0x02, 0x03};
+    crc::reset();
+    ASSERT_EQ(crc::calc(memory, 3), 0x55BC801D);
+    ASSERT_EQ(crc::sum(), 0x55BC801D);
+}
+
+TEST(CrcCalcTest, HandlesKnownCrcValues) {
+    uint8_t memory1[4] = {0xDE, 0xAD, 0xBE, 0xEF};
+    crc::reset();
+    ASSERT_EQ(crc::calc(memory1, 4), 0x7C9CA35A);
+    ASSERT_EQ(crc::sum(), 0x7C9CA35A);
+
+    uint8_t memory2[5] = {0x12, 0x34, 0x56, 0x78, 0x9A};
+    crc::reset();
+    ASSERT_EQ(crc::calc(memory2, 5), 0x3C4687AF);
+    ASSERT_EQ(crc::sum(), 0x3C4687AF);
+}
+
+}  // namespace
